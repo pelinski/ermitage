ab0e93c43dc7c772b46fe6eb8f0ee50036513b66
Teresa Pelinski <teresapelinski@gmail.com>
2020-04-03 19:21:08 +0200

    form helpers created, form updates new adition automathically

---
+++ front/src/lib/formHelpers.js
@@ -0,0 +1,24 @@
+import _ from "lodash";
+
+export const  handleInputChange = (e,data,setData) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setData({ ...data, [name]: value });
+  };
+
+
+export const handleSubmit = ({fields,data,apiFunction,setError,setChanges}) => {
+  const postObj = _.pick({ ...data }, fields);
+  apiFunction(postObj).then((res) => {
+    if (res.status != 200) {
+      console.log(res.data.message)
+      setError(res.data.message)
+    }
+    else {
+      console.log(res.data.message);
+    }
+    setChanges(true)
+  }).catch(e => console.log(e))
+
+
+};
--- front/src/pages/Dashboard.page.js
+++ front/src/pages/Dashboard.page.js
@@ -1,59 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { withProtected } from "../lib/protectRoute.hoc"
-import { getFolders, createFolder } from "../lib/dashboard.api";
 import { Link } from "react-router-dom"
+
 import { Field } from "../components/Form";
 import { FormButton } from "../components/Buttons"
+
+import { withProtected } from "../lib/protectRoute.hoc"
+import { getFolders, createFolder } from "../lib/dashboard.api";
+
+import { handleInputChange, handleSubmit } from "../lib/formHelpers";
+
+const Folder = ({ children }) => (<div className="folder"><div className="folderDetail" /><div className="folderContent">{children}</div></div>)
 
 
 const Page = () => {
   const [folders, setFolders] = useState([]);
+  const [error, setError] = useState("");
+  const [changes,setChanges] = useState(true);
   const [data, setData] = useState({
     folder: ""
   });
-  const [error, setError] = useState("");
-  
-  const handleInputChange = e => {
-    const value = e.target.value;
-    const name = e.target.name;
-    setData({ ...data, [name]: value });
-  };
 
-  const handleSubmit = () => {
-
-    const {folder} = { ...data };
-   
-    createFolder({folder}).then((res) => {
-      if (res.status != 200) {
-        console.log(res)
-        console.log(res.data.message)
-        setError(res.data.message)
-      }
-      else {
-        console.log(res.data.message);
-      }
-    }).catch(e => console.log(e))
-
-  };
+  useEffect(() => { getFolders().then(res => setFolders(res.data)).finally(setChanges(false)) }, [changes]);
 
 
-  useEffect(() => { getFolders().then(res => setFolders(res.data)) }, [])
   return (
-    <div>
+    <>
       <h1> These are your folders</h1>
-      {folders.map((e, i) => <Link to={e.path} key={i}>{e.folder} </Link>)}
+
+
+      <div className="folderWrapper">
+        {folders.map((e, i) => <Link to={e.path} key={i} ><Folder> {e.folder} </Folder> </Link>)}
+      </div>
+
 
       <form onSubmit={e => {
         e.preventDefault();
-        handleSubmit(data);
+        handleSubmit({ fields: ["folder"], data, apiFunction: createFolder, setError,setChanges });
       }}>
-        <Field field="folder" {...{ example: "project", data, handleInputChange }} />
-      {error}
-        <FormButton type="submit">
-          Add folder
-       </FormButton>
+        <Field field="folder" {...{ example: { folder: "project" }, data }} handleInputChange={(e) => handleInputChange(e, data, setData)} />
+        {error}
+        <FormButton type="submit"> Add folder </FormButton>
       </form>
-    </div>
+
+    </>
   )
 };
+
 export const DashboardPage = withProtected(Page);
