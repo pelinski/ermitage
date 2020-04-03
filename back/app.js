ed8b4850ca478bf75579446953e6fe1f54ff64ee
Teresa Pelinski <teresapelinski@gmail.com>
2020-04-03 21:32:30 +0200

    removed duplicated passport initialize

--- back/app.js
+++ back/app.js
@@ -59,10 +59,6 @@ app.use(
 );
 require("./passport")(app);
 
-
-
-app.use(passport.initialize());
-app.use(passport.session());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
